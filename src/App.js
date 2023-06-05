6f2da72eafe1151b6fab284ee76a518fc4d9ad7c
Haider Ali <133966815+haider863@users.noreply.github.com>
2023-06-05 15:37:26 +0500

    array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [inputEntry, setInputEntry] = useState("");
+  const [clickedIndex, setClickedIndex] = useState();
+  const [updatedValue, setUpdatedValue] = useState("");
+  const [updateBtn, setUpdateBtn] = useState(false);
+  const [da, setDa] = useState([]);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setDa([...da, inputEntry]); 
+    setInputEntry("");
+  };
+  function removeItem(index) {
+    let data = [...da];
+    data.splice(index, 1);
+    setDa(data);
+  }
+  function handleUpdateInput(e, index) {
+    let val = e.target.value;
+    setUpdatedValue(val);
+  }
+
+  function handleUpdateButton(index) {
+    let data = da;
+    data[index] = updatedValue;
+    setDa(data);
+    setClickedIndex("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleClick}>
+        <input
+          type="text"
+          value={inputEntry}
+          onChange={(e) => setInputEntry(e.target.value)}
+        ></input>
+        <input type="submit"></input>
+      </form>
+
+      {da.map((a, index) => (
+        <div key={index}>
+          <input
+            value={clickedIndex === index ? updatedValue : a}
+            disabled={clickedIndex === index ? false : true}
+            onChange={(e) => handleUpdateInput(e, index)}
+            type="text"
+          />
+          <button onClick={() => removeItem(index)}>Delete</button>
+
+          {clickedIndex === index ? (
+            <button onClick={() => handleUpdateButton(index)}>Update</button>
+          ) : (
+            <button onClick={() => setClickedIndex(index)}>Edit</button>
+          )}
+        </div>
+      ))}
+    </>
   );
 }
 
